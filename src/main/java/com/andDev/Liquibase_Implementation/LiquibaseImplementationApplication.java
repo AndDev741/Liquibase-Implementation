75665a3d0dd551b3f4b6057bbe1021249b7e6b15
andregoncalvesgentekai <andre.goncalves@gentek.ai>
2026-02-04 17:09:12 +0000

    project: Spring boot default start

---
+++ src/main/java/com/andDev/Liquibase_Implementation/LiquibaseImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.andDev.Liquibase_Implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andDev/Liquibase_Implementation/LiquibaseImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andDev.Liquibase_Implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
